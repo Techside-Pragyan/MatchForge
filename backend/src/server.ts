1c9d06545f09cf4e93145616e9fb211b5803590f
Pragyan Paramita Moharana <moharanapragyanparamita@gmail.com>
2026-04-26 22:56:19 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,12 @@
 import http from 'http';
 import mongoose from 'mongoose';
-import app from './app';
+import app from './app.js';
 import { Server } from 'socket.io';
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/matchforge';
 
-import { setupChatHandlers } from './socket/chatHandler';
+import { setupChatHandlers } from './socket/chatHandler.js';
 
 const server = http.createServer(app);
 
